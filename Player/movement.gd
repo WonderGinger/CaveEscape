43b4902f4fe9ab12ebf2b97def92ada857860ba4
Wonder Ginger <brandt.willems@gmail.com>
2017-10-17 20:42:23 -0700

    Worked to try to get movement to be momentum based, but it doesn't work correctly. Will revisit this later.

--- Player/movement.gd
+++ Player/movement.gd
@@ -1,34 +1,44 @@
-extends Node2D
+extends KinematicBody2D
 
-# class member variables go here, for example:
-# var a = 2
-# var b = "textvar"
 const MAX_MOVE_SPEED = 5
+const MAX_VECTOR_LENGTH = 20
+const VECTOR_INCREMENT = .5
 var speed = 1
-
-func _ready():
-	# Called every time the node is added to the scene.
-	# Initialization here
-	set_fixed_process(true)
-#	set_process(true)
-#	set_process_input(true)
-#	print(self)
 
 func _fixed_process(delta):
 	var move_direction = Vector2(0,0)
-	
-	var dpad_left = Input.is_action_pressed("player_move_left")
-	var dpad_right = Input.is_action_pressed("player_move_right")
-	var dpad_up  = Input.is_action_pressed("player_move_up")
-	var dpad_down = Input.is_action_pressed("player_move_down")
-	
-	if dpad_left:
+	var dpad = { 
+	'left':Input.is_action_pressed("player_move_left"), 
+	'right':Input.is_action_pressed("player_move_right"),
+	'up':Input.is_action_pressed("player_move_up"),
+	'down':Input.is_action_pressed("player_move_down") 
+	}
+	var key_pressed = dpad['left'] || dpad['right'] || dpad['up'] || dpad['down']
+		
+	if dpad['left'] && move_direction.x > -MAX_VECTOR_LENGTH:
 		move_direction += Vector2(-1,0)
-	if dpad_right:
+	if dpad['right'] && move_direction.x < MAX_VECTOR_LENGTH:
 	    move_direction += Vector2(1,0)
-	if dpad_up:
+	if dpad['up'] && move_direction.y < MAX_VECTOR_LENGTH:
 	    move_direction += Vector2(0,-1)
-	if dpad_down:
-	    move_direction += Vector2(0,1)
-	move(move_direction.normalized() * speed)
-	if speed < MAX_MOVE_SPEED: speed += 1
+	if dpad['down'] && move_direction.y > -MAX_VECTOR_LENGTH:
+		move_direction += Vector2(0,1)
+	
+#	if(key_pressed):
+#		if speed < MAX_MOVE_SPEED: speed += .1
+#	else: 
+#		if speed > 0: speed -= .1
+#		else: move_direction = Vector2(0,0)
+	move(move_direction.normalized() * MAX_MOVE_SPEED)
+#	move_direction += inc_vector_towards_zero(move_direction)
+
+func inc_vector_towards_zero(vector):
+	var result = Vector2(0,0)
+	if vector.x > 0: result += Vector2(-VECTOR_INCREMENT,0)
+	elif vector.x < 0: result += Vector2(VECTOR_INCREMENT,0)
+	elif vector.y > 0: result += Vector2(0,-VECTOR_INCREMENT)
+	elif vector.y < 0: result += Vector2(0,VECTOR_INCREMENT)
+	return result
+
+func _ready():
+	set_fixed_process(true)
