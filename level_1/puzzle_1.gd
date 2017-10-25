fd1d0687b6fbb50c1af098fbaa3a9830ab74e2a7
Wonder Ginger <brandt.willems@gmail.com>
2017-10-25 00:22:51 -0700

    This tutorial level will be easy, but it is a nightmare to code. I finished the algorithms for detecting a winner and such, and everything should work except that nothing happens when you win yet.

--- level_1/puzzle_1.gd
+++ level_1/puzzle_1.gd
@@ -1,39 +1,87 @@
 extends Node
 
-# http://mathforum.org/alejandre/magic.square/adler/adler.whatsquare.html
-# Used to calculate whether someone won.
-var magic_square = {
-	'1': 8, '2': 1, '3': 6,
-	'4': 3, '5': 5, '6': 7,
-	'7': 4, '8': 9, '9': 2
-}
-var tictactoe = {
-	'NW': false, 
-	'N': false,
-	'NE': false, 
-	'W': false, 
-	'C': false,
-	'E': false,
-	'SW': false,
-	'S': false, 
-	'SE': false
-}
+onready var tiles = [get_node("NW"), get_node("N"), get_node("NE"), 
+					 get_node("W"), get_node("C"), get_node("E"), 
+					 get_node("SW"), get_node("S"), get_node("SE")]
+var translate = {'0': [0,0], '1': [0,1], '2': [0,2],
+				 '3': [1,0], '4': [1,1], '5': [1,2],
+				 '6': [2,0], '7': [2,1], '8': [2,2]}
+var board = []
 var movecount = 0
 
 func _ready():
 	set_process(true)
+	for x in range(3):
+		board.append([])
+		for y in range(3):
+			board[x].append(0)
 
-func player_turn(element):
-	tictactoe[element.get_name()] = true
-	movecount += 1
-	if(movecount < 9):
-		get_node(ai_turn()).ai_move()
+func player_move(element):
+	for i in range(9):
+		if element == tiles[i] && movecount < 9:
+			if(!place(i, 1)):
+				ai_turn()
+			break
 
-#returns the name of the tic tac toe grid that an X was placed on.
 func ai_turn():
-	for i in tictactoe:
-		if(tictactoe[i] == false):
-			tictactoe[i] = true
-			movecount += 1
-			return i
+	while true:
+		randomize()
+		var rand_index = randi()%9
+		if tiles[rand_index].placeable:
+			tiles[rand_index].ai_move()
+			place(rand_index, -1)
+			break
 
+# state should be passed 1 for X, -1 for O, returns whether 
+func place(index, state):
+	movecount += 1
+	var t = translate[String(index)]
+	board[t[0]][t[1]] = state
+	return check_winner(t[0], t[1], state)
+
+# state should be passed 1 for X, -1 for O
+func check_winner(x, y, state):
+	#columns
+	print("Move: (%s, %s)" % [x, y])
+	print("COLUMNS")
+	for i in range(3):
+		print("Checking board[%s][%s] for %s, found: %s" % [i,y,state,board[i][y]])
+		if board[i][y] != state:
+			break
+		if(i == 2):
+			return true
+	
+	#rows
+	print("ROWS")
+	for i in range(3):
+		print("Checking board[%s][%s] for %s, found: %s" % [y,i,state,board[y][i]])
+		if board[x][i] != state:
+			break
+		if i == 2:
+			return true
+	
+	#diag
+	print("DIAG")
+	if(x == y):
+		for i in range(3):
+			print("Checking board[%s][%s] for %s, found: %s" % [i,i,state,board[i][i]])
+			if board[i][i] != state:
+				break
+			if i == 2:
+				return true
+	
+	#antidiag
+	print("ANTIDIAG")
+	if(x + y == 2):
+		for i in range(3):
+			print("Checking board[%s][%s] for %s, found: %s" % [i,2-i,state,board[i][2-i]])
+			if board[i][2-i] != state:
+				break
+			if i == 2:
+				return true
+	
+	#draw
+	if movecount == 9:
+		print("DRAW")
+		return true
+	return false
--- level_1/tic_tac_toe/grid_element.gd
+++ level_1/tic_tac_toe/grid_element.gd
@@ -2,25 +2,38 @@ extends Area2D
 
 var placeable = true
 var player = null
+var body_inside = false
+var position = [0,0]
 
 func _ready():
 	set_process(true)
 	connect("body_enter", self, "on_body_enter")
+	connect("body_exit", self, "on_body_exit")
 
 func _process(delta):
-	if Input.is_action_pressed("player_interact") && can_place_piece(player, self): 
-		get_node("piece").set_animation("X_piece")
-		get_parent().player_turn(self)
-		placeable = false
+	if Input.is_action_pressed("player_interact") && can_place_piece(): #can_place_piece(player, self): 
+		player_move()
 
-func can_place_piece(char, item):
-	if char:
-		if char.get_global_pos().distance_to(item.get_global_pos()) <= 40:
-			return placeable
+func can_place_piece():
+	if body_inside:
+		return placeable
 	return false
+
+func player_move():
+		get_node("piece").set_animation("X_piece")
+		placeable = false
+		get_parent().player_move(self)
 
 func ai_move():
 	get_node("piece").set_animation("O_piece")
+	placeable = false
 
 func on_body_enter( body ):
+	player = body
+	body_inside = placeable
+
+func on_body_exit(body):
+	body_inside = false
+
+func _on_Area2D_body_enter(body):
 	player = body
