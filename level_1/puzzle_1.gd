10dea89594248f8a8f731d4217a1278a04d0e129
Wonder Ginger <brandt.willems@gmail.com>
2017-10-25 01:00:52 -0700

    This is a buggy version of the tic tac toe algorithm with actually decent moves to a certain extent

--- level_1/puzzle_1.gd
+++ level_1/puzzle_1.gd
@@ -3,11 +3,13 @@ extends Node
 onready var tiles = [get_node("NW"), get_node("N"), get_node("NE"), 
 					 get_node("W"), get_node("C"), get_node("E"), 
 					 get_node("SW"), get_node("S"), get_node("SE")]
+onready var areas = []
 var translate = {'0': [0,0], '1': [0,1], '2': [0,2],
 				 '3': [1,0], '4': [1,1], '5': [1,2],
 				 '6': [2,0], '7': [2,1], '8': [2,2]}
 var board = []
 var movecount = 0
+var ai_next_move = []
 
 func _ready():
 	set_process(true)
@@ -15,11 +17,19 @@ func _ready():
 		board.append([])
 		for y in range(3):
 			board[x].append(0)
+	var tile_index = 0
+	for x in range(3):
+		areas.append([])
+		for y in range(3):
+			areas[x].append(tiles[tile_index])
+			tile_index += 1
+	print(areas)
 
 func player_move(element):
 	for i in range(9):
 		if element == tiles[i] && movecount < 9:
-			if(!place(i, 1)):
+			var t = translate[String(i)]
+			if(!place(t[0], t[1], 1)):
 				ai_turn()
 			else: get_node("/root/globals").goto_scene(globals.cave_2)
 			break
@@ -27,18 +37,23 @@ func player_move(element):
 func ai_turn():
 	while true:
 		randomize()
-		var rand_index = randi()%9
-		if tiles[rand_index].placeable:
-			tiles[rand_index].ai_move()
-			place(rand_index, -1)
-			break
+		var index = randi()%9
+		if tiles[index].placeable:
+#			print("ai next move: %s" % [ai_next_move])
+			if ai_next_move.size() != 0:
+				place(ai_next_move[0],ai_next_move[1], -1)
+			else: 
+				var t = translate[String(index)]
+				place(t[0], t[1], -1)
+		break
 
 # state should be passed 1 for X, -1 for O, returns whether 
-func place(index, state):
+func place(x,y , state):
 	movecount += 1
-	var t = translate[String(index)]
-	board[t[0]][t[1]] = state
-	return check_winner(t[0], t[1], state)
+	board[x][y] = state
+	if state == -1:
+		areas[x][y].ai_move()
+	return check_winner(x, y, state)
 
 # state should be passed 1 for X, -1 for O
 func check_winner(x, y, state):
@@ -49,7 +64,10 @@ func check_winner(x, y, state):
 		print("Checking board[%s][%s] for %s, found: %s" % [i,y,state,board[i][y]])
 		if board[i][y] != state:
 			break
-		if(i == 2):
+		if i == 1:
+			ai_next_move = [i+1, y]
+			print("ai's next move will be at ", ai_next_move)
+		if i == 2:
 			return true
 	
 	#rows
@@ -79,6 +97,7 @@ func check_winner(x, y, state):
 			if board[i][2-i] != state:
 				break
 			if i == 2:
+				print("won in column %s" % 2-i)
 				return true
 	
 	#draw
