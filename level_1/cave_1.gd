6c5999d0bfe9db4a24548493180d077efd85dca6
Wonder Ginger <brandt.willems@gmail.com>
2017-10-26 18:34:18 -0700

    everything for level 1 is complete except the art

--- level_1/cave_1.gd
+++ level_1/cave_1.gd
@@ -11,6 +11,9 @@ func _ready():
 func _fixed_process(delta):
 	if globals.puzzle_1_complete && open_door: 
 		get_node("Door").set_animation("door_open")
+		get_node("Switch").set_animation("switch_on")
 		open_door = false
 		get_node("move_cave_1").set_collision_mask(old_collision_mask)
 		get_node("move_cave_1").set_layer_mask(old_layer_mask)
+		get_node("move_puzzle_1").set_collision_mask(0)
+		get_node("move_puzzle_1").set_layer_mask(0)
